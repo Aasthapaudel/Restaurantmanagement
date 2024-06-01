3ccce70f759b7c6c550dd8fde2d3b1c7d509388d
Aastha <pdlpangali111@gmail.com>
2024-06-01 13:29:51 +0545

    guest page redirect to home done

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,6 +1,10 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
         {{ $logo }}
+        <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+            <a class="" href="{{url('/home')}}">Go to Site</a>
+
+        </div>
     </div>
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
